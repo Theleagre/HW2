520c228abdc7b564ba07b0f3119bcae0b97a9234
theleagre <grld@mail.ru>
2022-11-05 23:47:46 +1000

    Задание 1 в HW2

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        System.out.println("Задача2");
+        dog += 4;
+        System.out.println(dog);
+        cat += 4;
+        System.out.println(cat);
+        paper +=4;
+        System.out.println(paper);
+        System.out.println("Задача3");
+        dog -= 3.5;
+        System.out.println(dog);
+        cat -=1.6;
+        System.out.println(cat);
+        paper -= 7639;
+        System.out.println(paper);
+
+    }
+}
